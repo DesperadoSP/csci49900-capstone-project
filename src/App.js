dd4838929878b836d51dfe4911aa7bb25f98d4d6
segyoon95 <61797761+segyoon95@users.noreply.github.com>
2021-09-16 15:28:16 -0400

    Update App.js
    Tried making the navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState, Component} from 'react'
 import './App.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+        <div className = "navBar"> 
+          Trade Breath 
+          <button className = "navTabs"> Home </button>
+          <button className = "navTabs"> Products </button>
+          <button className = "navTabs"> About </button>
+        </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation></Navigation>
+      
     </div>
   );
 }
