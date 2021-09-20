698f19a67dc3433ba87d20623b67365943b4cd1f
segyoon95 <61797761+segyoon95@users.noreply.github.com>
2021-09-20 14:42:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
+/* eslint-disable */
+import { render } from '@testing-library/react';
 import React, {useState, Component} from 'react'
 import './App.css';
 
 class Navigation extends Component {
   render() {
     return (
-        <div className = "navBar"> 
-          Trade Breath 
-          <button className = "navTabs"> Home </button>
-          <button className = "navTabs"> Products </button>
-          <button className = "navTabs"> About </button>
+        <div>
+          <div className = {"navBar"}> 
+            Trade Breath 
+            <button className = {"navTabs"}> Home </button>
+            <button className = {"navTabs"}> Products </button>
+            <button className = {"navTabs"}> About </button>
+          </div>
+          <SearchBar className = {"searchBar"}></SearchBar>
         </div>
+          
+          
     );
   }
 }
 
 function App() {
   return (
-    <div className="App">
+    <div className = "App">
       <Navigation></Navigation>
-      
     </div>
   );
+}
+
+function SearchBar() {
+  return (
+    <table className = {"table"}>
+            <tr>
+                <td>
+                    <input type = {"text"} size= {"25"} className = {"input-sm"} placeholder = {"Search"}/>
+                </td>
+            </tr>
+        </table>
+  )
 }
 
 export default App;
