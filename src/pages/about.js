b0b1c55db795f120215be4ed4f9203fb8c8bf233
segyoon95 <alexdjun@gmail.com>
2021-09-20 22:00:21 -0400

    Revert "Update App.js"
    This reverts commit a533b88b10a77b120d6ade0f4953c4dbe0d92c38.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,23 @@
 
 import React from 'react';
+import './about.css';
   
 const About = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Center',
-        alignItems: 'Left',
-        height: '100vh'
-      }}
-    >
-      <h1>About Us</h1>
+    <div className = "container">
+      <div className = "aboutTitle">
+        <h1>About Us</h1>
+      </div>
+        <div className = "aboutContent">
+             <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+        </div>
+
+      <div className = "whoTitle">
+        <h1>Who We Are</h1>
+      </div>
+        <div className = "whoContent">
+             <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+        </div>
     </div>
   );
 };
