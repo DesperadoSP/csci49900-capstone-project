b8c723405ed125615c0af91e9832e89bbf07735a
segyoon95 <alexdjun@gmail.com>
2021-09-20 22:30:57 -0400

    About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,10 @@
 
 import React from 'react';
 import './about.css';
+import Adrian from './Adrian.png'
+import Alex from './Alex.png'
+import Diego from './Diego.png'
+import Edwin from './Edwin.png'
   
 const About = () => {
   return (
@@ -18,6 +22,13 @@ const About = () => {
         <div className = "whoContent">
              <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
         </div>
+        <div className = "peopleContainer">
+          <img className = "people" src = {Adrian} alt = "Adrian Ramirez"></img>
+          <img className = "people" src = {Alex} alt = "Alex Jun"></img>
+          <img className = "people" src = {Diego} alt = "Diego Kervabon"></img>
+          <img className = "people" src = {Edwin} alt = "Edwin Zhu"></img>
+        </div>
+        
     </div>
   );
 };
