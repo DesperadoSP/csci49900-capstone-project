773ca34cde076a6f55da806bc3ffe504df9c7fc8
segyoon95 <alexdjun@gmail.com>
2021-09-23 18:50:07 -0400

    ProductSearchFilterBar

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Form, FormControl, Button } from "react-bootstrap";
+import './products.css';
   
 const Home = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Center',
-        alignItems: 'Left',
-        height: '100vh'
-      }}
-    >
-      <h1>Our Products</h1>
+    <div>
+      <div className = "Title"><h1 className = "Title">Products</h1></div>
+      <Form inline id="productSearchBar">
+        <FormControl type="text" placeholder="Search" id="productSearchBar"/>
+        <FormControl type="text" placeholder="Filter" id="filterSearchBar"/>
+        <Button id="productSearchButton">
+          Search
+        </Button>   
+      </Form>
+        
     </div>
   );
 };
