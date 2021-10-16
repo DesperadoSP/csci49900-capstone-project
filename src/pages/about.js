9d69a9d4e73a3ef62402aa8d55c7babac9700ba3
segyoon95 <alexdjun@gmail.com>
2021-10-16 15:24:33 -0400

    About page update

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,25 +6,70 @@ import Diego from './Diego.png'
 import Edwin from './Edwin.png'
 
 import Navbar from '../components/Navbar';
-  
+
+import StockPic from './stock.jpeg'
+import HunterPic from './hunter.jpg'
+
 const About = () => {
   return (
     <div id = "content">
     <Navbar />
     <div className = "container">
       <div className = "aboutTitle">
-        <h1>About Us</h1>
+        <h1>TradeBreath</h1>
+      </div>
+        <img className = "stockPic" src = {StockPic} alt = "decoPic"></img>
+        <div className = "aboutContent">
+             <p> TradeBreath is a finanicial website which provides stock information such as: open, close, high low, volume of the stock with the line chart and a candlestick chart. <br></br> We want every investor to invest safe by providing the lastest news of the specific sotck along with the news from the stock market. </p>
+        </div>
+        
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+      {/* <div className = "chartTitle">
+        <h1>Charts</h1>
       </div>
         <div className = "aboutContent">
-             <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-        </div>
+           <p> Line chart is  </p>
+        </div> */}
 
       <div className = "whoTitle">
         <h1>Who We Are</h1>
       </div>
+        <img className = "hunterPic" src = {HunterPic} alt = "hunterPic"></img>
         <div className = "whoContent">
-             <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+             <p> We are stduent of Hunter College and this website is a project for the class CSCI 49900.  </p>
         </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+
         <div className = "peopleContainer">
           <img className = "people" src = {Adrian} alt = "Adrian Ramirez"></img>
           <img className = "people" src = {Alex} alt = "Alex Jun"></img>
